3960c5e86ab405b1ea0ce80dd7e6738038312eaf
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-09-01 14:14:13 +0200

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,5 @@
 import menuTemplate from "../templates/menu-tmp.hbs"
-import menuItems from "./menu.json"
+import menuItems from "../menu.json"
 
 const menuMarkup = createMenuItems(menuItems);
 const menuRef = document.querySelector('.js-menu');
